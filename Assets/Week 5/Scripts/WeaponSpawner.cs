ca0bc67728c73a5f8c6cb0bcf8fa5d5c3fc86428
Violet Li <livio@shernet.sheridancollege.ca>
2024-02-08 12:43:38 -0500

    Completed task 4b

---
+++ Assets/Week 5/Scripts/WeaponSpawner.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponSpawner : MonoBehaviour
+{
+    public GameObject weapon;
+    public void Spawn()
+    {
+        Instantiate(weapon);
+    }
+}
