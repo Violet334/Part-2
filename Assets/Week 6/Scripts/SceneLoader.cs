b82d756c1c9e82436520f942bcd7e7b59cfe3042
Violet Li <livio@shernet.sheridancollege.ca>
2024-02-15 10:10:15 -0500

    Completed task 1b

---
+++ Assets/Week 6/Scripts/SceneLoader.cs
@@ -0,0 +1,21 @@
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public int maxSceneIndex = 1;
+    public void LoadNextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = (currentSceneIndex + 1) % SceneManager.sceneCountInBuildSettings;
+        if(currentSceneIndex == maxSceneIndex)
+        {
+            nextSceneIndex = 0;
+        }
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+}
