b18cfd3b4cfdbfe1a105d45091c0c7cf112e6f14
Violet Li <livio@shernet.sheridancollege.ca>
2024-02-15 09:42:34 -0500

    Completed task 1a

--- Assets/Week 5/Scripts/HealthBar.cs
+++ Assets/Week 5/Scripts/HealthBar.cs
@@ -1,14 +1,20 @@
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
     public Slider slider;
 
-    public void TakeDamage(float damage)
+    public void TakeDamage(float value)
     {
-        slider.value -= damage;
+        slider.value -= value;
+    }
+
+    void SetHealth(float value)
+    {
+        slider.value = value;
     }
 }
--- Assets/Week 5/Scripts/ReplacementKnight.cs
+++ Assets/Week 5/Scripts/ReplacementKnight.cs
@@ -1,26 +1,28 @@
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
 
-public class Knight : MonoBehaviour
+public class ReplacementKnight : MonoBehaviour
 {
     Vector2 destination;
     Vector2 movement;
-    public float speed = 3;
+    public float speed = 3f;
+    bool clickingOnSelf;
+    public float maxHealth = 10;
+    public float health;
+    bool isDead;
+
     Rigidbody2D rb;
     Animator animator;
-    bool clickSelf = false;
-    public float health;
-    public float maxHealth = 5;
-    bool isDead = false;
-    public HealthBar healthBar;
-    // Start is called before the first frame update
-    void Start()
+
+    private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
         health = maxHealth;
+        isDead = false;
     }
 
     private void FixedUpdate()
@@ -28,44 +30,45 @@ public class Knight : MonoBehaviour
         if (isDead) return;
 
         movement = destination - (Vector2)transform.position;
+
         if (movement.magnitude < 0.1)
         {
             movement = Vector2.zero;
         }
+
         rb.MovePosition(rb.position + movement.normalized * speed * Time.deltaTime);
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetMouseButtonDown(0) && !clickSelf && !EventSystem.current.IsPointerOverGameObject())
-        {
-            destination = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        }
-        animator.SetFloat("Movement", movement.magnitude);
-
-        if (Input.GetMouseButtonDown(1))
-        {
-            animator.SetTrigger("Attack");
-        }
-    }
-
-    private void OnMouseDown()
+    private void Update()
     {
         if (isDead) return;
 
-        clickSelf = true;
-        SendMessage("TakeDamage", 1);
+        if (Input.GetMouseButtonDown(0) && !clickingOnSelf)
+        {
+            destination = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+
+        if (Input.GetMouseButtonDown(1) && !clickingOnSelf)
+        {
+            animator.SetTrigger("Attack");
+        }
+
+        animator.SetFloat("Movement", movement.magnitude);
+    }
+    private void OnMouseDown()
+    {
+        if (isDead) return;
+        clickingOnSelf = true;
+        SendMessage("TakeDamage", 2);
     }
     private void OnMouseUp()
     {
-        clickSelf = false;
+        clickingOnSelf = false;
     }
     public void TakeDamage(float damage)
     {
         health -= damage;
         health = Mathf.Clamp(health, 0, maxHealth);
-        if(health == 0)
+        if (health == 0)
         {
             isDead = true;
             animator.SetTrigger("Death");
@@ -73,7 +76,7 @@ public class Knight : MonoBehaviour
         else
         {
             isDead = false;
-            animator.SetTrigger("Damage");
+            animator.SetTrigger("TakeDamage");
         }
     }
 }
