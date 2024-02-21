15450508981de6b8bdd765164d01d4b0922c9fc1
Violet334 <148483747+Violet334@users.noreply.github.com>
2024-02-21 18:00:44 -0500

    created animated character

---
+++ Assets/Assignment/Scripts/Player.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    //declare variables
+    Vector2 destination;
+    Vector2 movement;
+    public float speed = 3;
+    Rigidbody2D rb;
+    Animator animator;
+    public float happiness;
+    public float maxHappiness = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //get components
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void FixedUpdate()
+    {
+        //character movement
+        movement = destination - (Vector2)transform.position;
+        //stop character figeting
+        if (movement.magnitude < 0.1)
+        {
+            movement = Vector2.zero;
+        }
+        rb.MovePosition(rb.position + movement.normalized * speed * Time.deltaTime);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        //update movement from mouse click
+        if (Input.GetMouseButtonDown(0))
+        {
+            destination = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+        animator.SetFloat("move", movement.magnitude);
+    }
+}
