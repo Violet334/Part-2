88ac46885a70ccd8d303165a1b204cc66f2b553c
Violet Li <livio@shernet.sheridancollege.ca>
2024-02-22 10:26:42 -0500

    Completed task 1b

---
+++ Assets/Week 7/Scripts/Player.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public SpriteRenderer player;
+    public Color selected;
+    public Color unselected;
+    private void Start()
+    {
+        player = GetComponent<SpriteRenderer>();
+        Selected(false);
+    }
+    public void Selected(bool isSelected)
+    {
+        if (isSelected)
+        {
+            player.color = selected;
+        }
+        else
+        {
+            player.color = unselected;
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        Selected(true);
+    }
+}
